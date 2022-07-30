55a02f45a2569d72e8c83f6eff2b3dfddc87c701
Gabriel Fernandes <gabgui2001@gmail.com>
2022-07-30 00:00:01 -0300

    start

---
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html', // local fire
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+
+export class NovaTransferenciaComponent{
+
+}
